d801d173e99f55c0e3fa8c2ff6fd58c7f1a8e736
Nghi <nghingothanhnghi@gmail.com>
2023-11-20 16:34:36 +0700

    update

--- src/components/searchbar/index.jsx
+++ src/components/searchbar/index.jsx
@@ -1,6 +1,55 @@
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { getPosts } from "../../api/http-common";
+import {createRoot} from 'react-dom/client'
+import Markdown from 'react-markdown'
+
 
 const SearchBar = ({ searchQuery, setSearchQuery }) => {
+    const [stateName, setStateName] = useState("AK");
+const [data,setData] = useState([]);
+const [stateData, setStateData] = useState();
+
+useEffect(() => {      
+    getPosts()
+        .then(response => response.data)
+        .then(responseData => {
+            setData(responseData)
+          })
+        .catch(err => {
+          console.log(err);
+        });
+      }, []);
+
+useEffect(() => {
+  setStateData(data.filter(i => i.state === stateName)[0]) 
+  
+},[data, stateName]);
+
+console.log(data, 'filter from axios')
+//     const [searchText, setSearchText] = useState("");
+//     useEffect(() => {
+//         const timer = setTimeout(() => {
+//             getPosts()
+//             .then((res) => {
+//                 setSearchText(res.data);
+//               console.log(res.data, 'Posts List Component');
+//             })
+//             .catch((err) => {
+//               console.log(err);
+//             })
+//             .finally(() => {
+//             });
+//         }, 1000);
+//         return () => clearTimeout(timer);
+//       }, []);
+    
+//   const filteredBooks = books.filter(
+//     ({ id, title }) =>
+//       id.toLowerCase().includes(searchText.toLowerCase()) ||
+//       title.toLowerCase().includes(searchText.toLowerCase())
+//   );
+
     const navigate = useNavigate();
     const onSubmit = () => {
         navigate(`?s=${searchQuery}`);
@@ -21,12 +70,21 @@ const SearchBar = ({ searchQuery, setSearchQuery }) => {
             <input
                 value={searchQuery}
                 onInput={(e) => setSearchQuery(e.target.value)}
+                // onChange={({ target }) => setData(target.value)}
                 type="text"
                 id="header-search"
                 placeholder="Search blog posts"
                 name="s"
             />
             <button type="submit">Search</button>
+            <ul>
+        {data.map(({ attributes, id }) => (
+          <li key={id}>
+            <strong>{attributes.title}</strong>
+   
+          </li>
+        ))}
+      </ul>
         </form>
     );
 };
--- src/pages/search-result.jsx
+++ src/pages/search-result.jsx
@@ -1,42 +1,71 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Announcer from '../components/announcer';
+import { getPosts } from '../api/http-common';
+
+// const posts = [
+//     { id: '1', name: 'This first post is about React' },
+//     { id: '2', name: 'This next post is about Preact' },
+//     { id: '3', name: 'We have yet another React post!' },
+//     { id: '4', name: 'This is the fourth and final post' },
+// ];
 
 
-const posts = [
-    { id: '1', name: 'This first post is about React' },
-    { id: '2', name: 'This next post is about Preact' },
-    { id: '3', name: 'We have yet another React post!' },
-    { id: '4', name: 'This is the fourth and final post' },
-];
-
-const filterPosts = (posts, query) => {
-    if (!query) {
-        return posts;
-    }
-
-    return posts.filter((post) => {
-        const postName = post.name.toLowerCase();
-        return postName.includes(query);
-    });
-};
 
 const SearchResult = () => {
+    const [stateName, setStateName] = useState("AK");
+    const [data,setData] = useState([]);
+    const [stateData, setStateData] = useState();
+    useEffect(() => {      
+        getPosts()
+            .then(response => response.data)
+            .then(responseData => {
+                setData(responseData)
+              })
+            .catch(err => {
+              console.log(err);
+            });
+          }, []);
+    
+    useEffect(() => {
+      setStateData(data.filter(i => i.state === stateName)[0]) 
+      
+    },[data, stateName]);
+    
+    console.log(data, 'filter from axios')
+
+
+    const filterPosts = (data, query) => {
+        if (!query) {
+            return data;
+        }
+    
+        return data.filter((post) => {
+            const postName = post.attributes.title.toLowerCase();
+            console.log(postName, "filter post name")
+            return postName.includes(query);
+        });
+    };
+
+
     const { search } = window.location;
     const query = new URLSearchParams(search).get('s');
     const [searchQuery] = useState(query || '');
-    const filteredPosts = filterPosts(posts, searchQuery);
-    console.log(query)
-    console.log(filteredPosts)
+    const filteredPosts = filterPosts(data, searchQuery);
+    console.log(query, 'query from defaul')
+    console.log(filteredPosts, 'filter Posts')
  
     return (
             <div className="SearchResult">
                 <Announcer
-                    message={`${filteredPosts.length} posts`}
+                    message={`${filteredPosts.length} data`}
                 />
                 <ul>
-                    {filteredPosts.map((post) => (
-                        <li key={post.id}>{post.name}</li>
+                    {filteredPosts.map((attributes, id) => (
+                        <li key={id}>
+                        <strong>{attributes.title}</strong>
+                      </li>
                     ))}
+                    
                 </ul>
             </div>
     );
